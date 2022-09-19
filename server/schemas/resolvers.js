a036423827e73dcad9bb3ae6d9f83a07c739fe26
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-09-19 11:42:04 -0700

    add graphql query and resolvers for thoughts, users

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,25 +1,46 @@
-const { User, Thought } = require('../models');
+const { User, Thought } = require("../models");
 
 const resolvers = {
-    Query: {
-         //methods get the same name of the query or 
-         //mutation they are resolvers for
+  Query: {
+    //methods get the same name of the query or
+    //mutation they are resolvers for
 
-         //pass parent as placeholder parameter. It won't be used, but we need 
-         //something in that first parameter's spot so we can access the 
-         //username argument from the second parameter
-         thoughts: async (parent, { username }) => {
-       
-        //use a ternary operator to check if username exists. If it does,
-        //set params to an object with a username key set to that value.
-        // If it doesn't, return an empty object.
-        const params = username ? { username } : {};
-        //pass params object, with or without any data in it, to our .find() 
-        //perform .find() on query thoughts and return data in descending 
-        //order using .sort()
-        return Thought.find(params).sort({ createdAt: -1 });
-      },
-    }
-  };
-  
-  module.exports = resolvers;
+    //pass parent as placeholder parameter. It won't be used, but we need
+    //something in that first parameter's spot so we can access the
+    //username argument from the second parameter
+    thoughts: async (parent, { username }) => {
+      //use a ternary operator to check if username exists. If it does,
+      //set params to an object with a username key set to that value.
+      // If it doesn't, return an empty object.
+      const params = username ? { username } : {};
+      //pass params object, with or without any data in it, to our .find()
+      //perform .find() on query thoughts and return data in descending
+      //order using .sort()
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+
+    //finding a single thought - destructure the _id argument value
+    //place into .findOne() to look up a single thought by its _id
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+
+    // get all users
+    users: async () => {
+      return User.find()
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+
+    // get a user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+  },
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,8 +13,13 @@ const { gql } = require("apollo-server-express");
 //defined thoughts query to "could" receive a parameter if we wanted.
 //the parameter would be identified as username and would be a String data type
 
+//define User
+
 //define query to return an array, as noted by []
 //around the returning data, Thought
+
+//create queries to retrieve a single thought by 
+//_id value, all users, and single user by username
 const typeDefs = gql`
   type Thought {
     _id: ID
@@ -32,8 +37,20 @@ const typeDefs = gql`
     username: String
   }
 
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
   type Query {
+    users: [User]
+    user(username: String!): User
     thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
   }
 `;
 
